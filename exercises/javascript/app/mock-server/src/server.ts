6ab8cbe53c18ef36edbe3daaff752565191ff020
David Knaack <david.knaack@sap.com>
2026-02-25 15:49:52 +0100

    fix(server): Handle errors when starting the mock server

--- exercises/javascript/app/mock-server/src/server.ts
+++ exercises/javascript/app/mock-server/src/server.ts
@@ -3,6 +3,10 @@ import { createLogger } from '@sap-cloud-sdk/util';
 const logger = createLogger('server');
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
-    logger.info(`Mock server started on port ${port}`);
+app.listen(port, (err) => {
+    if (err) {
+        logger.error(`Error starting mock server: ${err}`);
+    } else {
+        logger.info(`Mock server started on port ${port}`);
+    }
 });
